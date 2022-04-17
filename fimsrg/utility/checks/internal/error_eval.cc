3c1850723569ab75b81a3a347875d8a2df201bd4
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-04-17 16:34:02 +0200

    Add internal error evaluation

---
+++ fimsrg/utility/checks/internal/error_eval.cc
@@ -0,0 +1,27 @@
+// Copyright 2022 Matthias Heinz
+#include "fimsrg/utility/checks/internal/error_eval.h"
+
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+#include "fmt/core.h"
+#include "spdlog/spdlog.h"
+
+namespace fimsrg {
+namespace internal {
+
+std::string GenerateFullErrorMessage(const char* file, int line,
+                                     const char* msg) {
+  return fmt::format("({}:{}): {}", file, line, msg);
+}
+
+void CheckForError(bool val, std::string_view msg) {
+  if (!val) {
+    spdlog::error("Runtime error: {}", msg);
+    throw fimsrg::internal::RuntimeError(fmt::format("Runtime error: {}", msg));
+  }
+}
+
+}  // namespace internal
+}  // namespace fimsrg
---
+++ fimsrg/utility/checks/internal/error_eval.h
@@ -0,0 +1,30 @@
+// Copyright 2022 Matthias Heinz
+#ifndef FIMSRG_UTILITY_CHECKS_INTERNAL_ERROR_EVAL_H_
+#define FIMSRG_UTILITY_CHECKS_INTERNAL_ERROR_EVAL_H_
+
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+namespace fimsrg {
+namespace internal {
+
+// Exception class to signal general runtime error.
+class RuntimeError : public std::runtime_error {
+  // Inherit constructor
+  using std::runtime_error::runtime_error;
+};
+
+// Dress error message with file and line number.
+std::string GenerateFullErrorMessage(const char* file, int line,
+                                     const char* msg);
+
+// Check for error state (val).
+//
+// If in error state, log error and raise fatal exception.
+void CheckForError(bool val, std::string_view msg);
+
+}  // namespace internal
+}  // namespace fimsrg
+
+#endif  // FIMSRG_UTILITY_CHECKS_INTERNAL_ERROR_EVAL_H_
