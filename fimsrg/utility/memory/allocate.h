7b4e15c517deadcc5c631e6e1d9b6b31dce2bd2b
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-04-27 16:37:39 +0200

    Add information about 0-byte allocations to docstring

--- fimsrg/utility/memory/allocate.h
+++ fimsrg/utility/memory/allocate.h
@@ -29,18 +29,24 @@ void BarePooledDeallocate(void* ptr, std::size_t num_bytes);
 }  // namespace internal
 
 // Allocate a buffer for num_elems elements of type T.
+//
+// num_elems may be 0, in which case T* will be a null_ptr.
 template <typename T>
 T* Allocate(std::size_t num_elems) {
   return static_cast<T*>(fimsrg::internal::BareAllocate(num_elems * sizeof(T)));
 }
 
 // Free a buffer for num_elems elements of type T.
+//
+// num_elems may be 0.
 template <typename T>
 void Deallocate(T* ptr, std::size_t num_elems) {
   fimsrg::internal::BareDeallocate(ptr, num_elems * sizeof(T));
 }
 
 // Allocate a buffer for num_elems elements of type T.
+//
+// num_elems may be 0, in which case T* will be a null_ptr.
 template <typename T>
 T* PooledAllocate(std::size_t num_elems) {
   return static_cast<T*>(
@@ -48,6 +54,8 @@ T* PooledAllocate(std::size_t num_elems) {
 }
 
 // Free a buffer for num_elems elements of type T.
+//
+// num_elems may be 0.
 template <typename T>
 void PooledDeallocate(T* ptr, std::size_t num_elems) {
   fimsrg::internal::BarePooledDeallocate(ptr, num_elems * sizeof(T));
