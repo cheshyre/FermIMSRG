a1a33fb6833d70926fd19ca0c36435dc46782408
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-04-27 10:51:50 +0200

    Expand memory to include some tools to manage alignment.

---
+++ fimsrg/utility/memory/alignment.cc
@@ -0,0 +1,18 @@
+// Copyright 2022 Matthias Heinz
+#include "fimsrg/utility/memory/alignment.h"
+
+#include <cstdlib>
+
+#include "fimsrg/utility/memory/internal/alignment.h"
+
+namespace fimsrg {
+namespace detail {
+std::size_t BareRoundUpToMultipleOfAlignment(std::size_t num_bytes) {
+  using fimsrg::internal::min_alignment;
+  return ((num_bytes + min_alignment - 1) / min_alignment) * min_alignment;
+}
+}  // namespace detail
+
+std::size_t GetMinimumAlignment() { return fimsrg::internal::min_alignment; }
+
+}  // namespace fimsrg
---
+++ fimsrg/utility/memory/alignment.h
@@ -0,0 +1,35 @@
+// Copyright 2022 Matthias Heinz
+#ifndef FIMSRG_UTILITY_MEMORY_ALIGNMENT_H_
+#define FIMSRG_UTILITY_MEMORY_ALIGNMENT_H_
+
+#include <cstdlib>
+
+namespace fimsrg {
+namespace detail {
+
+// Rounds up a number of bytes to be a multiple of alignment.
+//
+// Prefer calling RoundUpToMultipleOfAlignment<T>(num_elems).
+std::size_t BareRoundUpToMultipleOfAlignment(std::size_t num_bytes);
+
+}  // namespace detail
+
+// Rounds up a number of elements of type T to correspond
+// to a multiple of alignment in memory.
+//
+// NOTE THAT THIS IS A NUMBER OF ELEMENTS, NOT A NUMBER OF BYTES.
+template <typename T>
+std::size_t RoundUpToMultipleOfAlignment(std::size_t num_elems) {
+  return fimsrg::detail::BareRoundUpToMultipleOfAlignment(num_elems *
+                                                          sizeof(T)) /
+         sizeof(T);
+}
+
+// Get the minimum alignment on the current platform.
+//
+// This should mostly be used for testing.
+std::size_t GetMinimumAlignment();
+
+}  // namespace fimsrg
+
+#endif  // FIMSRG_UTILITY_MEMORY_ALIGNMENT_H_
