ae2c63b6c0a7385354b67b6cf0d1d755ecd7c923
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-06-20 17:21:38 -0500

    Implement copy ctor

--- fimsrg/tensor/data/internal/data_buffer.cc
+++ fimsrg/tensor/data/internal/data_buffer.cc
@@ -8,16 +8,19 @@
 
 namespace fimsrg {
 namespace internal {
+
+static double* AllocateAndCopyBuffer(const double* src_buffer_ptr,
+                                     std::size_t src_buffer_size);
+
 DataBuffer::DataBuffer() {}
 
 DataBuffer::DataBuffer(std::size_t num_elems)
     : num_elems_(num_elems),
       data_ptr_(fimsrg::PooledAllocate<double>(num_elems_)) {}
 
-DataBuffer::DataBuffer(const DataBuffer& other) : DataBuffer() {
-  // TODO(mheinz): implement
-  (void)other;
-}
+DataBuffer::DataBuffer(const DataBuffer& other)
+    : num_elems_(other.size()),
+      data_ptr_(AllocateAndCopyBuffer(other.data(), other.size())) {}
 
 DataBuffer::DataBuffer(DataBuffer&& other) : DataBuffer() {
   // TODO(mheinz): implement
@@ -49,5 +52,17 @@ double& DataBuffer::at(std::size_t index) {
   Expects(index < size());
   return data_ptr_[index];
 }
+
+double* AllocateAndCopyBuffer(const double* src_buffer_ptr,
+                              std::size_t src_buffer_size) {
+  double* new_buffer_ptr = fimsrg::PooledAllocate<double>(src_buffer_size);
+
+  for (std::size_t i = 0; i < src_buffer_size; i += 1) {
+    new_buffer_ptr[i] = src_buffer_ptr[i];
+  }
+
+  return new_buffer_ptr;
+}
+
 }  // namespace internal
 }  // namespace fimsrg
