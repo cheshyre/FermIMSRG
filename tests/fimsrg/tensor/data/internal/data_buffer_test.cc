5cafe103392fd54b8e66232971cf0a3fff07d393
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-06-20 16:39:44 -0500

    Add some tests for the ctor

--- tests/fimsrg/tensor/data/internal/data_buffer_test.cc
+++ tests/fimsrg/tensor/data/internal/data_buffer_test.cc
@@ -2,12 +2,40 @@
 #include "fimsrg/tensor/data/internal/data_buffer.h"
 
 // IWYU pragma: no_include <built-in>
+#include <cstddef>
+#include <initializer_list>
 
 #include "catch2/catch.hpp"
 
+using fimsrg::internal::DataBuffer;
+
 TEST_CASE("Test default constructor.") {
-  fimsrg::internal::DataBuffer data_buffer;
+  DataBuffer data_buffer;
 
   REQUIRE(data_buffer.size() == 0);
   REQUIRE(data_buffer.data() == nullptr);
+}
+
+TEST_CASE("Test standard constructor and size().") {
+  for (const std::size_t size : {0, 1, 2, 3, 4, 8, 24, 64, 1000}) {
+    DataBuffer data_buffer(size);
+
+    REQUIRE(data_buffer.size() == size);
+  }
+}
+
+TEST_CASE("Test standard constructor and data() for nonempty.") {
+  for (const std::size_t size : {1, 2, 3, 4, 8, 24, 64, 1000}) {
+    DataBuffer data_buffer(size);
+
+    REQUIRE(data_buffer.data() != nullptr);
+  }
+}
+
+TEST_CASE("Test standard constructor and data() for empty.") {
+  for (const std::size_t size : {0}) {
+    DataBuffer data_buffer(size);
+
+    REQUIRE(data_buffer.data() == nullptr);
+  }
 }
